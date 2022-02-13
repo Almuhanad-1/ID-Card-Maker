55ee806191ab297c157a57e1733e76e18194b964
Almuhanad Alnihmy <68535829+Almuhanad-1@users.noreply.github.com>
2022-02-13 16:59:47 +0300

    Add files via upload

--- jscode.js
+++ jscode.js
@@ -1,26 +1,23 @@
 function idCard() {
-   document.getElementById('idCardDiv').style.display = "block";
 
    var firstName = document.getElementById('first-name').value;
    var lastName = document.getElementById('last-name').value;
    var address = document.getElementById('address').value;
-
-   var postFullName = document.getElementById('post-full-name');
-   postFullName.innerHTML = firstName + " " + lastName;
-   var postAddress = document.getElementById('post-address');
-   postAddress.innerHTML = address;
-
-
    var age = document.getElementById('age').value;
    var phoneNumber = document.getElementById('phone-num').value;
 
+
+   var postFullName = document.getElementById('post-full-name');
+   var postAddress = document.getElementById('post-address');
    var postAge = document.getElementById('post-age');
    var postPhoneNumber = document.getElementById('post-phone-number');
 
+
+   postFullName.innerHTML = firstName + " " + lastName;
+   postAddress.innerHTML = address;
+
    var numberArray = [];
    numberArray.push(age, phoneNumber);
-
-
 
    for (let i = 0; i < numberArray.length; i++) {
       if (numberArray[i] <= 100 & numberArray[i] != 0) {
@@ -32,18 +29,22 @@ function idCard() {
       else {
 
       }
-
    }
 
+   document.getElementById('idCardDiv').style.display = "block";
    document.getElementById('idCardDiv').scrollIntoView({ behavior: 'smooth' });
 
 }
 
 function resetCard() {
-   document.getElementById('idCardDiv').style.display = "none";
    window.scroll({
       top: 0,
       left: 0,
+      behavior: 'smooth'
    });
+   setTimeout(function () {
+      document.getElementById('idCardDiv').style.display = "none";
+   }, (0.1 * 1000));
+   // document.getElementById('idCardDiv').style.display = "none";
 
 }
